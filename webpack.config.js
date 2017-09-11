9ac9a43a9b448fdb5b3bc2dc052374744b8245f3
Dan Siberry <dansiberry1@gmail.com>
2017-09-11 12:55:03 +0200

    removed webpack fonts

--- webpack.config.js
+++ webpack.config.js
@@ -47,11 +47,6 @@ module.exports = {
           'css-loader',
           'sass-loader',
         ]
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [
-        'url-loader' ]
       }
     ]
   },
@@ -62,4 +57,3 @@ module.exports = {
     })
   ]
 }
-
